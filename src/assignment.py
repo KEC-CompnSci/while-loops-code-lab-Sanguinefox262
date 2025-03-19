f1e83b006d7d4ce4c157bf876ac7aca7a0853e71
Sanguinefox262 <27portek@retsdstudent.ca>
2025-03-19 19:38:54 +0000

    feyhwfeauk

--- src/assignment.py
+++ src/assignment.py
@@ -9,6 +9,14 @@ Replace this with your actual assignment content.
 # Write your code for Task 1 below:
 # TODO: Write a while loop that counts by 2s from 2 to 50 and prints each number
 
+count = 0
+while count < 50:
+    count += 1
+
+    if count % 2 == 0:
+        print(count)
+    else:
+        continue
 print("Task 1 complete!\n")
 
 # Task 2: Print a 5x5 grid of # symbols using nested while loops
@@ -17,6 +25,8 @@ print("Task 1 complete!\n")
 # Each row should have 5 # symbols
 # There should be 5 rows in total
 #remember to print without a new line use print("message", end="")
-
-
+rows = 0
+while rows <5:
+    print("#####")
+    rows += 1
 print("Task 2 complete!")
